d12b971a4ef4925267660966f8e2496a560dbfb3
hirorich <ch.dim7ir@gmail.com>
2022-12-26 01:54:42 +0900

    動的にスタイル変更

--- angular-layout/src/app/layout/main-layout/main-layout.component.ts
+++ angular-layout/src/app/layout/main-layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 import { ResizeObserverService } from 'src/app/service/resize-observer.service';
 
 @Component({
@@ -11,6 +12,14 @@ export class MainLayoutComponent implements AfterViewInit {
   @Input() sideWidth: number = 0;
 
   private el: ElementRef;
+  private elHead: HTMLElement | undefined;
+  private elContent: HTMLElement | undefined;
+  private elSide: HTMLElement | undefined;
+  private elMain: HTMLElement | undefined;
+  private elFoot: HTMLElement | undefined;
+
+  private heightHead: number = 0;
+  private heightFoot: number = 0;
 
   constructor(
     private resizeObserverService: ResizeObserverService,
@@ -20,17 +29,38 @@ export class MainLayoutComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    let elHead: HTMLElement = <HTMLElement> this.el.nativeElement.children[0];
-    let elContent: HTMLElement = <HTMLElement> this.el.nativeElement.children[1];
-    let elSide: HTMLElement = <HTMLElement> elContent.children[0];
-    let elMain: HTMLElement = <HTMLElement> elContent.children[1];
+    this.elHead = <HTMLElement> this.el.nativeElement.children[0];
+    this.elContent = <HTMLElement> this.el.nativeElement.children[1];
+    this.elSide = <HTMLElement> this.elContent.children[0];
+    this.elMain = <HTMLElement> this.elContent.children[1];
+    this.elFoot = <HTMLElement> this.el.nativeElement.children[2];
 
-    //let observable = this.resizeObserverService.createHeightResizeObservable(elHead);
-    //observable.subscribe((val) => this.setContentHeight(elContent, val));
+    of(this.sideWidth).subscribe((val) => this.configContent());
+    this.resizeObserverService.createHeightResizeObservable(<HTMLElement> this.elHead).subscribe((val) => {
+      this.heightHead = val;
+      this.configSide();
+    });
+    this.resizeObserverService.createHeightResizeObservable(<HTMLElement> this.elFoot).subscribe((val) => {
+      this.heightFoot = val;
+      this.configSide();
+      this.configMain();
+    })
   }
 
-  private setContentHeight(elContent: HTMLElement, headerHeight: number): void {
-    elContent.style.height = `calc(100% - ${headerHeight}px)`;
+  private configContent(): void {
+    let el: HTMLElement = <HTMLElement> this.elContent;
+    el.style.gridTemplateColumns = `${this.sideWidth}px calc(100% - ${this.sideWidth}px)`;
+  }
+
+  private configSide(): void {
+    let el: HTMLElement = <HTMLElement> this.elSide;
+    el.style.top = `${this.heightHead}px`;
+    el.style.height = `calc(100vh - ${this.heightHead}px - ${this.heightFoot}px)`;
+  }
+
+  private configMain(): void {
+    let el: HTMLElement = <HTMLElement> this.elMain;
+    el.style.marginBottom = `${this.heightFoot}px`;
   }
 
 }
