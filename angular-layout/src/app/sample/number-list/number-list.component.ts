325c07720b69b0b5d1dd618d3aac6047113248e7
hirorich <ch.dim7ir@gmail.com>
2022-12-29 15:26:36 +0900

    入力欄作成

--- angular-layout/src/app/sample/number-list/number-list.component.ts
+++ angular-layout/src/app/sample/number-list/number-list.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-number-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './number-list.component.html',
   styleUrls: ['./number-list.component.css']
 })
